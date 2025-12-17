c0ef0dde5951f328c52836e158dfea0c4fc2e033
Thomas Rice <thomasrice7@yahoo.com>
2025-12-17 02:33:18 -0500

    removed dupe

--- src/main.rs
+++ src/main.rs
@@ -2,60 +2,7 @@ use crate::cube::*;
 
 mod cube;
 
-fn rubiks_maneuver(cube: &mut Cube) {
-    cube.apply_rotations(&[
-        Rotate::Rp,
-        Rotate::L,
-        Rotate::Fp,
-        Rotate::Rp,
-        Rotate::L,
-        Rotate::Dp,
-        Rotate::Rp,
-        Rotate::L,
-        Rotate::B,
-        Rotate::B,
-        Rotate::R,
-        Rotate::Lp,
-        Rotate::Dp,
-        Rotate::R,
-        Rotate::Lp,
-        Rotate::Fp,
-        Rotate::R,
-        Rotate::Lp,
-        Rotate::U,
-        Rotate::U,
-    ]);
-    // cube.apply_rotations(&[
-    //     Rotate::Mp,
-    //     Rotate::Up,
-    //     Rotate::Mp,
-    //     Rotate::Up,
-    //     Rotate::Mp,
-    //     Rotate::Up,
-    //     Rotate::Up,
-    //     // second half
-    //     Rotate::M,
-    //     Rotate::Up,
-    //     Rotate::M,
-    //     Rotate::Up,
-    //     Rotate::M,
-    //     Rotate::Up,
-    //     Rotate::Up,
-    // ]);
-}
-
 // double rotates dont matter
 // single rotates swap the move direction
 
-fn main() {
-    let mut cube = Cube::solved();
-    cube.rotate(Rotate::R);
-    cube.rotate(Rotate::x);
-    cube.rotate(Rotate::U);
-    cube.print();
-
-    let mut cube2 = Cube::solved();
-    cube2.rotate(Rotate::R);
-    cube2.rotate(Rotate::F);
-    cube2.print();
-}
+fn main() {}
