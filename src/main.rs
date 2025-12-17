8719188a5a662e60803b883db945007678b01474
Thomas Rice <thomasrice7@yahoo.com>
2025-12-16 23:22:34 -0500

    starter code for cube representation and some moves

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use crate::cube::*;
+
+mod cube;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cube = Cube::solved();
+    cube.rotate(Rotate::R);
+    cube.print();
+    cube.rotate(Rotate::U);
+    cube.print();
+    cube.rotate(Rotate::RR);
+    cube.print();
+    cube.rotate(Rotate::D);
+    cube.print();
 }
